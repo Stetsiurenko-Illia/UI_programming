8e66590bde4d26bbdba1d71a69b413a6fd7d5a9d
Your Name <you@example.com>
2025-04-04 20:01:40 +0300

    Add real-time task sharing

--- todo-list-app/src/components/Tasks.js
+++ todo-list-app/src/components/Tasks.js
@@ -375,7 +375,16 @@ function Tasks() {
                     className="btn btn-outline-primary btn-sm"
                     onClick={() => handleShareClick(task.id)}
                   >
-                    ➤
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-share"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5m-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3" />
+                    </svg>
                   </button>
                 </div>
               </div>
@@ -394,7 +403,16 @@ function Tasks() {
                       className="btn btn-outline-success"
                       onClick={() => handleShareSubmit(task.id)}
                     >
-                      ✈
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-send"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
+                      </svg>
                     </button>
                   </div>
                 </div>
